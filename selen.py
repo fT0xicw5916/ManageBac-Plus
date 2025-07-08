8ffc8e8a1ae290263497e0e9f8be76e52a6d329b
fT0xicw5916 <epic.ft0xicw.5916@gmail.com>
2025-07-08 22:28:21 +0800

    Added functionality to get all past tasks of a certain subject. NOT TESTED!! INCOMPLETE!!

--- selen.py
+++ selen.py
@@ -50,6 +50,51 @@ class ManagebacDriver:
             btn_log.click()
         self.logger.info(f"Login success (Username = '{username}', Password = '{password}', {"MS" if microsoft else "MB"})")
 
+    def get_past_tasks(self, subject):
+        self.driver.get("https://huijia.managebac.cn/student")
+
+        unfiltered_li_classes = self.driver.find_elements(By.XPATH, self.classes_xpath)[:-1]
+        name_and_url = None
+        for unfiltered_li_class in unfiltered_li_classes:
+            unfiltered_link = unfiltered_li_class.find_element(By.XPATH, 'a')
+            unfiltered_text = unfiltered_link.find_element(By.XPATH, "span").get_attribute("innerHTML")
+
+            if unfiltered_text == subject:
+                name_and_url = (unfiltered_text, unfiltered_link.get_attribute("href"))
+                break
+
+        if name_and_url is None:
+            self.logger.error(f"Subject not found when trying to get past tasks for {subject}")
+            return None
+
+        self.driver.get(name_and_url[1] + "/core_tasks")
+        tasks_list_divs = self.driver.find_elements(By.XPATH, self.tasks_xpath)[2:]
+        tasks_list_divs = tasks_list_divs[::-1]  # Old task on top, latest task bottom
+
+        past_tasks = {}
+        for div in tasks_list_divs:
+            name = div.find_element(By.XPATH, "div[1]/div[2]/h4/a").get_attribute("innerHTML")
+            score_s = None
+
+            try:  # Score task
+                score_s = div.find_element(By.XPATH, "div[2]/div/div/div").get_attribute("innerHTML")
+            except:  # Not a score task (Score task and/or Completed)
+                try:  # Score task w/ completed
+                    score_s = div.find_element(By.XPATH, "div[2]/div[1]/div/div").get_attribute("innerHTML")
+                except:  # Completed
+                    past_tasks[name] = 1
+                    continue
+
+            if score_s == "N/A":  # N/A task
+                continue
+            elif score_s is None or score_s == "":  # Not assessed yet
+                continue
+            score_l = score_s.split(" / ")
+            score = float(score_l[0]) / float(score_l[1][:-4])
+            past_tasks[name] = score
+
+        return past_tasks
+
     def get_task_num(self, subject, category):
         self.driver.get("https://huijia.managebac.cn/student")
 
@@ -68,14 +113,13 @@ class ManagebacDriver:
             return None
 
         self.driver.get(name_and_url[1] + "/core_tasks")
-        tasks_list_divs = self.driver.find_elements(By.XPATH, self.tasks_xpath)
+        tasks_list_divs = self.driver.find_elements(By.XPATH, self.tasks_xpath)[2:]
 
         task_num = 0
         for div in tasks_list_divs:
-            if "fusion-card-item short-assignment" in div.get_attribute("class"):
-                tags = [i.get_attribute("innerHTML") for i in div.find_elements(By.XPATH, "div[1]/div[2]/div/div/*")[1:-1]]
-                if category[:category.find(' ')] in tags:
-                    task_num += 1
+            tags = [i.get_attribute("innerHTML") for i in div.find_elements(By.XPATH, "div[1]/div[2]/div/div/*")[1:-1]]
+            if category[:category.find(' ')] in tags:
+                task_num += 1
 
         return task_num
 
