2efc2a56985e85a21171a417b442679da57ea6b6
fT0xicw5916 <epic.ft0xicw.5916@gmail.com>
2025-11-12 00:04:59 +0800

    Fixed login_retry.py so that it actually works

--- scripts/login_retry.py
+++ scripts/login_retry.py
@@ -13,6 +13,8 @@ def read_logs(path, start_date, end_date):
             break
     for i in range(-1, -len(li) - 1, -1):
         if f"2025-{end_date}" in li[i]:
+            if i == -1:
+                break
             li = li[:i+1]
             break
 
@@ -20,28 +22,41 @@ def read_logs(path, start_date, end_date):
     for line in li:
         if "app.cache - Caching GPA data for" in line:
             info = line[line.find('(')+1:line.find(')')].split(", ")
-            r.append((info[0], info[1]))
+            try:
+                r.append((info[0], info[1]))
+            except Exception:
+                pass
 
     return r
 
 
 def main():
     li = read_logs("../logs.log", "11-03", "11-11")
+    print(len(li))
     for i in li:
+        print(i)
+
         cookies = {
             "username": i[0],
             "password": i[1],
             "microsoft": '0'
         }
-        requests.get("http://managebac.me/load_grades", cookies=cookies)
-        time.sleep(30)
+        try:
+            requests.get("http://managebac.me/load_grades", cookies=cookies)
+            time.sleep(30)
+        except Exception:
+            pass
+
         cookies = {
             "username": i[0],
             "password": i[1],
             "microsoft": '1'
         }
-        requests.get("http://managebac.me/load_grades", cookies=cookies)
-        time.sleep(30)
+        try:
+            requests.get("http://managebac.me/load_grades", cookies=cookies)
+            time.sleep(30)
+        except Exception:
+            pass
 
 
 if __name__ == "__main__":
